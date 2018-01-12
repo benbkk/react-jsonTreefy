c8d0315045bdc8009ffef67471cd1c28eb8b8d3a
Ben Anggoro <ben@benworkandplay.com>
2018-01-12 23:11:46 +0700

    Fixed jest failed on resolve

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppWrapper from './StyledApp';
 
-import MainStage from '../MainStage';
+import MainStage from 'components/MainStage';
 
 const App = () => (
     <AppWrapper className={'app-wrapper'}>
