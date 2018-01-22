5d0e78cd158603651ac0aec0ae043991b647e416
Ben Anggoro <ben@benworkandplay.com>
2018-01-23 04:44:36 +0800

    Added some more finishing styles

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,43 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardMedia, CardTitle, CardText } from 'react-toolbox/lib/card';
 
-const style = {
-  width: '350px',
-  marginLeft: '20px',
-  marginTop: '20px',
-  display: 'inline-block'
-};
-
-class ErrorBoundary extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false
-    };
-  }
-
-  render() {
-    if(this.state.hasError) {
-      return (
-        <Card style={style}>
-          <CardMedia
-            aspectRatio="wide"
-            image="https://cdn.dribbble.com/users/1078347/screenshots/2799566/oops.png"
-          />
-          <CardTitle
-            title="Sorry Something went wrong!!!"
-            subtitle="Error catched by error boundary of react 16"
-          />
-        </Card>
-      );
+class ErrorBoundaries extends Component {
+    static propTypes = {
+        children: PropTypes.node,
     }
-    return this.props.children;
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        };
+    }
+
+    componentDidCatch(error, info) {
+      // Display fallback UI
+      this.setState({ hasError: true });
+      // You can also log the error to an error reporting service
+      console.log(error, info);
+    }
+
+    render() {
+        if(this.state.hasError) {
+            return (
+                <h1>Oops... Something went wrong</h1>
+            );
+        }
+        return this.props.children;
+    }
 }
 
-export default ErrorBoundary;
+export default ErrorBoundaries;
